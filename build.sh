b2849780cbc553d2adc6f247b13c4dc1acf4e6aa
Sam Kaufman <emrysk@gmail.com>
2023-11-19 12:31:30 -0800

    build.sh: `set -e`

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+set -e
+
 # Run all scripts which begin with an integer in alphanumeric order.
 for script in $(ls -1v *.sh | grep '^[0-9]' | sort -n); do
     echo "Running $script"
