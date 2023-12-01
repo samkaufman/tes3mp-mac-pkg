98819a5b45ff62879c54dce92c876474c0748ba7
Sam Kaufman <emrysk@gmail.com>
2023-12-01 05:15:29 -0800

    Fix for Headers-stripping.

--- 41-minimize-app.sh
+++ 41-minimize-app.sh
@@ -3,10 +3,8 @@ set -ex
 . ./_common.sh
 
 # Remove Headers folders from frameworks.
-# This will print some "No such file or directory" warnings for the aliases,
-# but that's fine.
 find -L testbox/TES3MP.app/Contents/Frameworks/*.framework -name "Headers" \
-    -type d -exec rm -rf {} \;
+    -type d -exec rm -rf {} +
 
 # After modifying the link paths in a previous step, it should now be safe to
 # remove symlinks to dylibs. Binaries should reference the dylibs directly.
